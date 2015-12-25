65e4f4ae4dbe5ed1c8fc1fdfc0b58c691af94099
Md. Rana Mahmud <ranamd0@gmail.com>
2015-12-25 12:26:45 +0600

    Longest common subsequence length finding program completed

---
+++ LongestCommonSubsequence_sp.cpp
@@ -0,0 +1,42 @@
+#include <cstring>
+#include <iostream>
+#define MAXC 1000
+using namespace std;
+
+char A[MAXC],B[MAXC];
+int lenA,lenB;
+int dp[MAXC][MAXC];
+bool visited[MAXC][MAXC];
+
+int calcLCS(int i,int j)
+{
+    if(A[i]=='\0' or B[j]=='\0') return 0;
+    if(visited[i][j]) return dp[i][j];
+
+    int ans = 0;
+
+    if(A[i]==B[j])
+        ans = 1+calcLCS(i+1,j+1);
+    else
+    {
+        int val1 = calcLCS(i+1,j);
+        int val2 = calcLCS(i,j+1);
+        ans = max(val1,val2);
+
+    }
+    visited[i][j] = 1;
+    dp[i][j] =ans;
+    return dp[i][j];
+
+
+}
+
+int main()
+{
+
+    cin>>A>>B;
+    lenA = strlen(A);
+    lenB = strlen(B);
+    cout<<calcLCS(0,0)<<endl;
+
+}
